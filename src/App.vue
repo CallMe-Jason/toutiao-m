cab6423ff1813d59c1deacd5b152f2a111c404a8
CallMe-Jason <2872213719@qq.com>
2020-12-11 20:12:56 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <h1>黑马头条</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'App',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="less"></style>
